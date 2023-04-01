2e286f7dbd50fc95ef7156c2d6b22f28cf3d7f4b
ElnurBDa <121972600+ElnurBDa@users.noreply.github.com>
2023-04-01 22:03:15 +0400

    Delete lazy-worker-server/dist directory

--- lazy-worker-server/dist/like/like.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { LikeService } from './like.service';
-export declare class LikeController {
-    private likeService;
-    constructor(likeService: LikeService);
-    likeArticle(req: any): void;
-}
--- lazy-worker-server/dist/users/users.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersService } from './users.service';
-export declare class UsersController {
-    private usersService;
-    constructor(usersService: UsersService);
-    addInterest(req: any): Promise<import("./user.entity").User>;
-    removeInterest(req: any): Promise<import("./user.entity").User>;
-    getInterests(req: any): Promise<string[]>;
-    getUser(req: any): Promise<import("./user.entity").User>;
-}
